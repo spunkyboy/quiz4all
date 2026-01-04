8b349b7dc18b7361c6cc72190c5596d421b3c55a
frank Osei <frankosei@franks-MBP.powerhub>
2026-01-04 18:43:39 +0000

    Fix jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const jwtSecret= process.env.JWT_SECRET;
 const User = require('../models/User');
 const Admin = require('../models/Admin');
 const rateLimit = require('express-rate-limit'); // Use for rate-limiting 
@@ -9,7 +10,6 @@ const router = express.Router();
 const inputValidator = require('validator');
 const sendEmailReq = require('../utils/sendEmail');
 
-// const jwtSecret= process.env.JWT_SECRET;
 
 //sign in limiter
 const siginLimiter = rateLimit({
